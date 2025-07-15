35ad5dd68c9f7bcc14fd98712444853c0378ba98
Eric Tobin <eatobin@gmail.com>
2025-07-15 16:53:43 -0500

    progress

--- Day02/src/App/Program.cs
+++ Day02/src/App/Program.cs
@@ -1,36 +1,44 @@
 ﻿namespace App;
 
-public struct Person(string name, int age)
+public class Intcode(int pointer, int[] memory)
 {
-    public string Name = name;
-    public int Age = age;
-
-    public void Speak() => Console.WriteLine($"My Age: {Age}");
+    public int Pointer { get; set; } = pointer;
+    public int[] Memory { get; } = memory;
 }
 
-public static class Application
+internal static class Program
 {
-    private static void Main()
+    public static void Main()
     {
-        // Create  struct instance and initialize by using "new".
-        // Memory is allocated on thread stack.
-        var p1 = new Person("Alex", 9);
-        Console.WriteLine($"p1 Name = {p1.Name} Age = {p1.Age}");
+        var ic = new Intcode(1, [11]);
+        Console.WriteLine($"{ic.Pointer}");
+        Console.WriteLine($"{ic.Memory[0]}");
 
-        // Create  new struct object. Note that struct can be initialized
-        // without using "new".
-        var p2 = p1;
+        ic.Pointer = 2;
+        ic.Memory[0] = 222;
 
-        // Assign values to p2 members.
-        p2.Name = "Spencer";
-        p2.Age = 7;
-        Console.WriteLine($"p2 Name = {p2.Name} Age = {p2.Age}");
+        Console.WriteLine($"{ic.Pointer}");
+        Console.WriteLine($"{ic.Memory[0]}");
 
-        // p1 values remain unchanged because p2 is  copy.
-        Console.WriteLine($"p1 Name = {p1.Name} Age = {p1.Age}");
-
-        var p9 = new Person("Me", 99);
-        Console.WriteLine($"p9 Name = {p9.Name} Age = {p9.Age}");
-        p9.Speak();
+        // // Create  struct instance and initialize by using "new".
+        // // Memory is allocated on thread stack.
+        // var p1 = new Person("Alex", 9);
+        // Console.WriteLine($"p1 Name = {p1.Name} Age = {p1.Age}");
+        //
+        // // Create  new struct object. Note that struct can be initialized
+        // // without using "new".
+        // var p2 = p1;
+        //
+        // // Assign values to p2 members.
+        // p2.Name = "Spencer";
+        // p2.Age = 7;
+        // Console.WriteLine($"p2 Name = {p2.Name} Age = {p2.Age}");
+        //
+        // // p1 values remain unchanged because p2 is  copy.
+        // Console.WriteLine($"p1 Name = {p1.Name} Age = {p1.Age}");
+        //
+        // var p9 = new Person("Me", 99);
+        // Console.WriteLine($"p9 Name = {p9.Name} Age = {p9.Age}");
+        // p9.Speak();
     }
 }
